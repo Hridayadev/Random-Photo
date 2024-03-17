9383d7a6385434b1433740bc6da0a917873c1e41
Hridaya Dev Dhungana <hridayadev.dhungana@gmail.com>
2024-03-18 00:08:10 +0545

    added

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ btn.addEventListener("click", () => {
     image.src = `https://picsum.photos/300?random=${i}`;
     i++;
 });
-
+image.addEventListener("click", () => {
+    image.classList.toggle("full-size");
+});
 downloadLink.addEventListener("click", () => {
     downloadLink.href = image.src;
 });
